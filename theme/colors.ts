5d0bc655f7637834ee43a1ca5440b6dd2d8a046d
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-08-31 23:23:35 +0100

    chore : setup color theme

---
+++ theme/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  white: "#FFFFFF",
+  gray1: "#FEF2EA",
+  gray2: "#D9D9D9",
+  primary: "#FB8F3E",
+  secondary: "#FDD868",
+  primary2: "#E56B1E",
+  primary3: "#F95C08",
+};

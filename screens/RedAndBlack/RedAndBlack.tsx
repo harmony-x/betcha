b24a4defa80e9472bb9f33c574cc57d9813dc052
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-09-01 10:09:04 +0100

    feat : build red and black game

--- screens/RedAndBlack/RedAndBlack.tsx
+++ screens/RedAndBlack/RedAndBlack.tsx
@@ -11,16 +11,27 @@ import {
 import BlackBtnBg from "$svgs/BlackBtnBg";
 import CancelBtnBg from "$svgs/CancelBtnBg";
 import ConfirmBtnBg from "$svgs/ConfirmBtnBg";
+import PlayAgainBtnBg from "$svgs/PlayAgainBtnBg";
 import RedBtnBg from "$svgs/RedBtnBg";
+import Trophy from "$svgs/Trophy";
 import React from "react";
 import { useTheme } from "styled-components";
 import { RedAndBlackLeftPane, RedAndBlackWrapper } from "./RedAndBlack.styles";
+import { Choice, Game } from "./RedAndBlack.types";
 
 const RedAndBlack = () => {
   const theme = useTheme() as any;
   const [range, setRange] = React.useState<number>(0);
   const [isOpenModal, setIsOpenModal] = React.useState<boolean>(false);
-  const [choice, setChoice] = React.useState<"" | "red" | "black">("");
+  const [choice, setChoice] = React.useState<Choice>("");
+  const [game, setGame] = React.useState<Game>({
+    status: "init",
+    correctChoice: "",
+    amount: range * 2,
+  });
+
+  const { status, correctChoice, amount } = game;
+
   return (
     <RedAndBlackWrapper>
       <Flex flexDir="column" align="center" justify="center" gap="87px">
@@ -28,43 +39,118 @@ const RedAndBlack = () => {
           RED & BLACK
         </HeadingOne>
         <Flex width="max-content" gap="69px" align="center">
-          <RedAndBlackLeftPane />
+          <RedAndBlackLeftPane
+            backgroundImage={
+              status === "init" ||
+              status === "processing" ||
+              correctChoice === ""
+                ? "bcha_showcase.png"
+                : correctChoice === "red"
+                ? "red_card.png"
+                : correctChoice === "black"
+                ? "black_card.png"
+                : "bcha_showcase.png"
+            }
+          />
           <Flex flexDir="column" gap="50px" align="center">
-            <BetHeading betAmount={range} width="343px" height="93px" />
-            <RangeSlider range={range} setRange={setRange} width="411px" />
-            <Flex gap="13px">
-              <Button
-                onClick={() => {
-                  if (!!range) {
-                    setChoice("red");
-                    setIsOpenModal(true);
-                  }
-                }}
-                icon={<RedBtnBg />}
-                text={
-                  <Span size="2.4rem" lineHeight="31px">
-                    RED
-                  </Span>
-                }
-                height="66px"
-                width="200px"
+            {status === "init" ||
+            status === "processing" ||
+            correctChoice === "" ? (
+              <BetHeading
+                text={`BET ${range} BCHA`}
+                width="343px"
+                height="93px"
               />
-              <Button
-                onClick={() => {
-                  if (!!range) {
-                    setChoice("black");
-                    setIsOpenModal(true);
-                  }
-                }}
-                icon={<BlackBtnBg />}
-                text={
-                  <Span size="2.4rem" lineHeight="31px">
-                    BLACK
-                  </Span>
-                }
-                height="66px"
-                width="200px"
+            ) : correctChoice === "red" ? (
+              <BetHeading
+                color={theme.colors.gray1}
+                backgroundColor={theme.colors.red}
+                text="RED"
+                width="321px"
+                height="87.03px"
               />
+            ) : correctChoice === "black" ? (
+              <BetHeading
+                color={theme.colors.white}
+                backgroundColor={theme.colors.black}
+                text="BLACK"
+                width="321px"
+                height="87.03px"
+              />
+            ) : null}
+            {status !== "complete" ? (
+              <RangeSlider range={range} setRange={setRange} width="411px" />
+            ) : choice !== "" && correctChoice === choice ? (
+              <Flex gap="20px" align="center">
+                <Span size="3rem" lineHeight="48px">
+                  YOU WON
+                </Span>
+                <Trophy />
+                <Span size="3rem" lineHeight="48px">
+                  {amount} BCHA
+                </Span>
+              </Flex>
+            ) : choice !== "" && correctChoice !== choice ? (
+              <Span size="3rem" lineHeight="48px">
+                SORRY, YOU CHOSE {choice.toUpperCase()}!
+              </Span>
+            ) : null}
+            <Flex gap="13px" justify="center">
+              {status !== "complete" ? (
+                <>
+                  <Button
+                    onClick={() => {
+                      if (!!range) {
+                        setChoice("red");
+                        setIsOpenModal(true);
+                      }
+                    }}
+                    icon={<RedBtnBg />}
+                    text={
+                      <Span size="2.4rem" lineHeight="31px">
+                        RED
+                      </Span>
+                    }
+                    height="66px"
+                    width="200px"
+                  />
+                  <Button
+                    onClick={() => {
+                      if (!!range) {
+                        setChoice("black");
+                        setIsOpenModal(true);
+                      }
+                    }}
+                    icon={<BlackBtnBg />}
+                    text={
+                      <Span size="2.4rem" lineHeight="31px">
+                        BLACK
+                      </Span>
+                    }
+                    height="66px"
+                    width="200px"
+                  />
+                </>
+              ) : (
+                <Button
+                  onClick={() => {
+                    setGame({
+                      status: "init",
+                      correctChoice: "",
+                      amount: range * 2,
+                    });
+                    setRange(0);
+                  }}
+                  icon={<PlayAgainBtnBg />}
+                  text={
+                    <Span size="2.4rem" lineHeight="31px">
+                      PLAY AGAIN
+                    </Span>
+                  }
+                  height="66px"
+                  width="278.79px"
+                />
+              )}
             </Flex>
           </Flex>
         </Flex>
@@ -112,6 +198,16 @@ const RedAndBlack = () => {
               width="200px"
             />
             <Button
+              onClick={() => {
+                setGame({
+                  status: "complete",
+                  amount: range * 2,
+                  correctChoice: parseInt((Math.random() * 2).toString())
+                    ? "black"
+                    : "red",
+                });
+                setIsOpenModal(false);
+              }}
               icon={<ConfirmBtnBg />}
               text={
                 <Span size="2.4rem" lineHeight="31px">

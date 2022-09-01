543d66356a2fb2a190713a2848d82e8c416ee1c9
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-09-01 10:08:17 +0100

    style : update styles

--- screens/RedAndBlack/RedAndBlack.styles.ts
+++ screens/RedAndBlack/RedAndBlack.styles.ts
@@ -1,15 +1,16 @@
 import RedBtn from "$svgs/RedBtnBg";
 import styled from "styled-components";
+import { RedAndBlackLeftPaneProps } from "./RedAndBlack.types";
 
 export const RedAndBlackWrapper = styled.div`
   width: 100%;
 `;
 
-export const RedAndBlackLeftPane = styled.div`
+export const RedAndBlackLeftPane = styled.div<RedAndBlackLeftPaneProps>`
   position: relative;
   min-width: 304px;
   height: 369px;
-  background-image: url("/bcha_showcase.png");
+  background-image: ${({ backgroundImage }) => `url(/${backgroundImage})`};
   background-size: contain;
   background-position: center;
   background-repeat: no-repeat;

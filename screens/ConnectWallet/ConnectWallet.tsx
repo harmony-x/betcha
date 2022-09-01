efe4036ca713b2c6f91729909a271297fcd264e5
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-09-01 13:28:52 +0100

    feat : integrate connect wallet feature

--- screens/ConnectWallet/ConnectWallet.tsx
+++ screens/ConnectWallet/ConnectWallet.tsx
@@ -1,12 +1,32 @@
 import Button from "$components/Button/Button";
 import { Span } from "$components/Typography/Typography.styles";
+import { Web3Provider } from "@ethersproject/providers";
+import { useWeb3React } from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
 import Image from "next/image";
+import React from "react";
 import { ConnectWalletWrapper } from "./ConnectWallet.styles";
 
 const ConnectWallet = () => {
+  const injectedConnector = new InjectedConnector({
+    // supportedChainIds: [80001],
+  });
+  const { chainId, account, activate, active, library } =
+    useWeb3React<Web3Provider>();
+  React.useEffect(() => {
+    if (active && chainId !== 80001) {
+      // shows everytime
+      console.log("Only polygon testnet is supported.");
+    }
+  }, [active, chainId]);
+  const onClick = () => {
+    // where the connection happens
+    activate(injectedConnector);
+  };
   return (
     <ConnectWalletWrapper>
       <Button
+        onClick={onClick}
         icon={
           <Image
             width="636px"

88599a8cfb5c448bf439acb99f23645d4d5c0138
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-09-01 02:33:05 +0100

    style : remove background from main layout to body

--- layouts/Main/Main.styles.ts
+++ layouts/Main/Main.styles.ts
@@ -2,16 +2,12 @@ import styled from "styled-components";
 
 export const StyledMain = styled.main`
   width: 100%;
+  height: 100vh;
+  padding: 54px 120px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   flex-direction: column;
-  width: 100%;
-  height: 100vh;
-  background-image: url("/Game Background.png");
-  background-position: center;
-  background-size: cover;
-  padding: 54px 120px;
 `;
 
 export const StyledMainTop = styled.div`

a45c43d2704b661e08573ffff5bc657b31795e16
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-09-01 02:35:22 +0100

    chore : update global color theme

--- theme/colors.ts
+++ theme/colors.ts
@@ -1,9 +1,12 @@
 export const colors = {
   white: "#FFFFFF",
+  black: "#191B1F",
   gray1: "#FEF2EA",
   gray2: "#D9D9D9",
   primary: "#FB8F3E",
   secondary: "#FDD868",
   primary2: "#E56B1E",
   primary3: "#F95C08",
+  primary4: "#FC9D39",
+  red: "#EA3034",
 };

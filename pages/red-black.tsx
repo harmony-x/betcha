4e45e424428d7b4d7608fc75480adccf75a4b960
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2022-09-01 12:24:16 +0100

    chore : add bet history modal

--- pages/red-black.tsx
+++ pages/red-black.tsx
@@ -1,10 +1,14 @@
+import BetHistoryTable from "$components/BetHistoryTable/BetHistoryTable";
+import ModalTwo from "$components/ModalTwo/ModalTwo";
 import Main from "$layouts/Main/Main";
 import RedAndBlack from "$screens/RedAndBlack/RedAndBlack";
 import BackIcon from "$svgs/BackIcon";
 import type { NextPage } from "next";
 import Head from "next/head";
+import React from "react";
 
 const RedAndBlackPage: NextPage = () => {
+  const [isOpenHistoryModal, setIsOpenHistoryModal] = React.useState(false);
   return (
     <div>
       <Head>
@@ -15,9 +19,19 @@ const RedAndBlackPage: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Main buttonIcon={<BackIcon />}>
+      <Main
+        buttonIcon={<BackIcon />}
+        onHistoryClick={() => setIsOpenHistoryModal(true)}
+      >
         <RedAndBlack />
       </Main>
+      <ModalTwo
+        text="BET HISTORY"
+        isOpen={isOpenHistoryModal}
+        setIsOpen={setIsOpenHistoryModal}
+      >
+        <BetHistoryTable />
+      </ModalTwo>
     </div>
   );
 };
